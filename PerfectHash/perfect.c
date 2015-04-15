6392f0df6cc298f5c557c2f4f09352e60a347ded
Michelle Bergeron <mrb113@case.edu>
2015-04-14 23:55:10 -0700

    Create bucket linked list

--- PerfectHash/perfect.c
+++ PerfectHash/perfect.c
@@ -8,13 +8,19 @@ int perfectHash(kvp* input, int* lookuptable, int* hashtable) {
 		return 0;
 	}
 
-	int tablesize = nextPowerOfTwo(length(input)); 
-
+	const int tablesize = nextPowerOfTwo(length(input)); 
 	
+	// Create array of buckets
+	struct bucket* buckets = malloc(tablesize * sizeof(struct bucket*));
+	if (buckets == NULL) {
+		free(buckets);
+		return 0;
+	}
+
 	// TODO 1. create buckets by regular hashing
 	for (int i = 0; i < length(input); i++) {
 		int slot = (int)hash(input[i].key, 0) & (tablesize - 1);
-		// add input[i]
+		
 	}
 	
 	// TODO 2. sort buckets
@@ -22,6 +28,23 @@ int perfectHash(kvp* input, int* lookuptable, int* hashtable) {
 	// TODO 4. When found, add the values into the hash table
 	// TODO 5. insert seed into lookuptable[(int)hash(bucket[0].key, 0) & (length(lookuptable) - 1)]; 
 
+	return 1; 
+}
+
+/* Adds a key/value pair node to the beginning of the bucket */
+int addNodeToBucket(p_bucket b, uint key, uint value) {
+
+	p_kvp head = b->keyvalue; 
+    p_kvp new_head = (p_kvp) malloc (sizeof (kvp));
+
+	if (new_head == NULL) {
+		return 0; 
+	}
+    new_head->key = key;
+    new_head->next = head;
+
+	// Increase bucket size
+	b->size++; 
 	return 1; 
 }
 
@@ -41,6 +64,7 @@ uint lookup(uint key, int* lookuptable, p_kvp hashtable) {
 
 /* Round up to the next power of 2 */
 int nextPowerOfTwo(int v) {
+
 	v--;
 	v |= v >> 1;
 	v |= v >> 2;
--- PerfectHash/perfect.h
+++ PerfectHash/perfect.h
@@ -9,10 +9,11 @@ typedef unsigned int uint;
 /* 2^30: Largest signed integer power of 2 */
 #define MAX_INPUT 1073741824
 
-/* Key/value pair */
+/* Key/value pair node*/
 typedef struct KEY_VALUE_PAIR {
 	uint key; 
-	uint value; 
+	uint value;
+	struct KEY_VALUE_PAIR *next; 
 } kvp, *p_kvp;
 
 /* Bucket contains list of key/value pairs that collide with original hash function */
@@ -26,3 +27,4 @@ uint hash(uint a, uint seed);
 int nextPowerOfTwo(int v);
 uint lookup(uint key, int* lookuptable, p_kvp hashtable); 
 int perfectHash(kvp* input, int* lookuptable, int* hashtable);
+int addNodeToBucket(p_bucket b, uint key, uint value);
