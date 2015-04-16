3ecc9041ef4f1e677b2c39d24fc03a083091ab91
Michelle Bergeron <mrb113@case.edu>
2015-04-15 22:24:23 -0700

    Add some printing

--- PerfectHash/test.c
+++ PerfectHash/test.c
@@ -3,13 +3,29 @@
 
 void main() {
 	// Add tests here as needed
-	
+
+	getchar(); 
 }
 
 void printlookuptable(int* hashtable){
 	// TODO 
 }
 
-void printhashtable(kvp* hashtable){
+void printhashtable(p_kvp hashtable){
 	// TODO 
+}
+
+void printbucketlist(p_bucket bucketlist) {
+	for (int i = 0; i < length(bucketlist); i++) {		
+		printbucket(&bucketlist[i]);
+	}
+}
+
+void printbucket(p_bucket bucket){
+	printf("Printing bucket contents: \n"); 
+	p_kvp k = bucket->keyvalue;
+	while (k != NULL) {
+		printf("Key: %d Value: %d\n", k->key, k->value); 
+		k = k->next; 
+	}
 }
--- PerfectHash/test.h
+++ PerfectHash/test.h
@@ -1,4 +1,6 @@
 #include "perfect.h"
 
 void printlookuptable(int* hashtable);
-void printhashtable(kvp* hashtable);
+void printhashtable(p_kvp hashtable);
+void printbucket(p_bucket bucket);
+void printbucketlist(p_bucket bucketlist);
