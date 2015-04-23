c3a0fcdf8a8b34fe01d1d41698a858b04db585f6
Michelle Bergeron <mrb113@case.edu>
2015-04-22 23:35:02 -0700

    Adding basic tests/examples

--- PerfectHash/test.c
+++ PerfectHash/test.c
@@ -1,45 +1,88 @@
 #include "test.h"
-
 void main() {
 	// Michelle's hacky "test as you go" setup
 	// No real test framework yet
-
+	 
 	struct timeb start, end;
 	int diff;
 
+	uint kernelbasekeys[36954];
+	FILE *pKernelList;
+	if (fopen_s(&pKernelList, "C:\\Users\\Michelle\\Documents\\PerfectHash\\kernelbase.txt", "r") != 0) {
+		printf("file read failed\n");
+		return;
+	}
+	for (int i = 0; i < 36954; i++){
+		uint number;
+		fscanf_s(pKernelList, "%x", &number);
+		kernelbasekeys[i] = 63580000 - number;
+		if (feof(pKernelList)){
+			break;
+		}
+	}	
+
 	// Round up the current # of keys for link /dump /headers /loadconfig mshtml.dll (~37000)
-	int length = 40000;
-	uint input[40000];
-	uint values[40000];
+	int length = 36954;
+	printf("Number of keys: %d\n", length);
+	uint values[36954];
 	for (int i = 0; i < length; i++) {
-		input[i] = i; 
-		values[i] = i+1; 
+		
+		values[i] = i; 
 	}
 	
 	lookup lookuptable = CreateEmptyLookupTable(length);
 	assert(lookuptable.tablesize != -1);
 
 	ftime(&start);
-	assert(GeneratePerfectHash(input, lookuptable, length) != FAILURE);
+	assert(GeneratePerfectHash(kernelbasekeys, lookuptable, length) != FAILURE);
 	ftime(&end);
 
 	diff = (int)(1000.0 * (end.time - start.time)+ (end.millitm - start.millitm));
-	printf("Time taken in milliseconds to generate perfect hash function: %u\n", diff);
-	printf("Inserting keys into hash table\n");
+	printf("Time taken in milliseconds to generate perfect hash function: %u\n\n", diff);
+	printf("Inserting keys into hash table where input[i]'s key = i...\n");
 	uint *hashtable = malloc(lookuptable.tablesize * sizeof(uint));
 	for (int i = 0; i < length; i++) {
-		Insert(input[i], values[i], lookuptable, hashtable);
+		Insert(kernelbasekeys[i], values[i], lookuptable, hashtable);
 	}
-	printf("Looking up value at key 5090: \n");
-	int key = input[5090]; 
+	printf("Done inserting.\n\n");
+	printf("Looking up value of key input[30]: \n");
+	int key = kernelbasekeys[30]; 
 	int val = Lookup(key, lookuptable, hashtable);
 	printf("Found value %d\n", val);
+	printf("Timing lookup of every key:\n");
+	ftime(&start);
+	for (int i = 0; i < length; i++) {
+		Lookup(kernelbasekeys[i], lookuptable, hashtable);
+	}
+	ftime(&end);
+	diff = (int)(1000.0 * (end.time - start.time) + (end.millitm - start.millitm));
+	printf("Time taken in milliseconds to lookup every key: %u\n\n", diff);
 
-	// Values are all key+1
-	assert(val == key+1);
+	// Values are all key+
+	calculatePercentageEmpty(lookuptable);
+	printf("Done.\n");
 	FreeLookupTable(lookuptable);
 	free(hashtable);
 	getchar(); 
+}
+
+// Use keys from MSHTML.dll
+
+// Use hash function 1
+// Calculate percentage of 1s in lookup table
+
+// Use hash function 2
+// Calculate percentage of 1s in lookup table
+void calculatePercentageEmpty(lookup lookuptable) {
+	printf("Calculating the percentage of nonzero elements in lookup table of size: %d\n", lookuptable.tablesize);
+	int count = 0; 
+	for (int i = 0; i < lookuptable.tablesize; i++) {
+		if (lookuptable.table[i] > 0) {
+			count++;
+		}
+	}
+	double percentage = (double)count / (double)lookuptable.tablesize;
+	printf("%f \n", percentage);
 }
 
 void printlookuptable(lookup* lookuptable, int size){
--- PerfectHash/test.h
+++ PerfectHash/test.h
@@ -5,3 +5,4 @@ void main();
 void printlookuptable(lookup* lookuptable, int size);
 void printbucketlist(p_bucket bucketlist, int size);
 void printbucket(p_bucket bucket);
+void calculatePercentageEmpty(lookup lookuptable);
